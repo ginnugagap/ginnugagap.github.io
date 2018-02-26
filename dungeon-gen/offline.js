1988eb2c6a0b0aa9a9ad102807219c3566851952
ivan <ivan.hu@advantech.com.tw>
2018-02-26 12:47:44 +0800

    init

---
+++ dungeon-gen/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1519620373,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tile-sheet0.png",
+		"images/dot-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"dungeongen.js"
+	]
+}
